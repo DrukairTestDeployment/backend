1349d63b1e96008aa8ba5e7d6881c18617c7f740
tashiwangyel <bajayfs@gmail.com>
2025-06-11 17:57:28 +0600

    new feature

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const agentRoutes = require('./routes/agentRoutes');
 const commisionRoutes = require('./routes/commisionRoutes')
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://helistaging.drukair.com.bt',
     credentials: true
 }));
 app.use(express.static('public'))
@@ -39,12 +39,214 @@ app.use("/api/agents", agentRoutes);
 app.use("/api/commision", commisionRoutes);
 
 app.post('/cancelrmapayment', (req, res) => {
-    if(req.body === null){
-        res.status(200).json({ message: 'Api called' });
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Confirmation</title>
+</head>
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f9f9f9;">
+    <div style="max-width: 600px; margin: 20px auto; padding: 20px; border: 1px solid #ccc; background-color: #fff; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+        <h2 style="text-align: center; color: #333;">PAYMENT CONFIRMATION (<span style="color: #E87111;">DRUKAIR HELI-RESERVATION</span>)</h2>
+        <hr style="border: none; border-top: 1px solid #ccc; margin: 10px 0;">
         
-    }
-    console.log("im called")
-    res.status(200).json({ message: 'Payment cancelled successfully' });
+        <div style="margin-bottom: 20px;">
+            <div style="background-color: #1E3166; color: white; padding: 10px; font-weight: bold; text-transform: uppercase;">Agent Information</div>
+            <div style="padding: 10px;">
+                <p style="margin: 5px 0;"><strong>Name:</strong> <span id="name"></span></p>
+                <p style="margin: 5px 0;"><strong>Agent CID:</strong> <span id="cid"></span></p>
+                <p style="margin: 5px 0;"><strong>Booking Reference:</strong> <span id="id"></span></p>
+                <p style="margin: 5px 0;"><strong>Issued Through:</strong> <span id="type"></span></p>
+            </div>
+        </div>
+
+        <div>
+            <div style="background-color: #1E3166; color: white; padding: 10px; font-weight: bold; text-transform: uppercase;">Fare Information</div>
+            <div style="padding: 10px;">
+                <p style="margin: 5px 0;"><strong>Transaction Status:</strong> Payment Cancelled</p>
+                <p style="margin: 5px 0;"><strong>Flight Fare:</strong> <span id="price"></span> <span>BTN</span></p>
+                <p style="margin: 5px 0;"><strong>Transaction Description:</strong> DrukAir Heli Reservation System Payment</p>
+            </div>
+        </div>
+        <div style="text-align: center; margin-top: 20px;">
+            <button 
+                onclick="window.location.href='/'" 
+                style="background-color: #1E3166; color: white; border: none; padding: 10px 20px; border-radius: 5px; font-size: 16px; cursor: pointer;">
+                OK
+            </button>
+        </div>
+    </div>
+</body>
+<script>
+    document.addEventListener("DOMContentLoaded", async function () {
+        const id = sessionStorage.getItem("bID");
+        try {
+            const getbooking = await axios({
+                method: "GET",
+                url: \`https://heli.drukair.com.bt/api/bookings/\${id}\`
+            });
+    
+            const booking = getbooking.data.data;
+            document.getElementById("name").textContent = booking.agent_name;
+            document.getElementById("cid").textContent = booking.agent_cid;
+            document.getElementById("id").textContent = booking.bookingID;
+            document.getElementById("type").textContent = booking.booking_type;
+            document.getElementById("price").textContent = booking.bookingPriceBTN;
+        } catch (err) {
+            console.log(err);
+        }
+    });      
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js"></script>
+</html>
+`);
+});
+
+app.post('/failurermapayment', (req, res) => {
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Confirmation</title>
+</head>
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f9f9f9;">
+    <div style="max-width: 600px; margin: 20px auto; padding: 20px; border: 1px solid #ccc; background-color: #fff; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+        <h2 style="text-align: center; color: #333;">PAYMENT CONFIRMATION (<span style="color: #E87111;">DRUKAIR HELI-RESERVATION</span>)</h2>
+        <hr style="border: none; border-top: 1px solid #ccc; margin: 10px 0;">
+        
+        <div style="margin-bottom: 20px;">
+            <div style="background-color: #1E3166; color: white; padding: 10px; font-weight: bold; text-transform: uppercase;">Agent Information</div>
+            <div style="padding: 10px;">
+                <p style="margin: 5px 0;"><strong>Name:</strong> <span id="name"></span></p>
+                <p style="margin: 5px 0;"><strong>Agent CID:</strong> <span id="cid"></span></p>
+                <p style="margin: 5px 0;"><strong>Booking Reference:</strong> <span id="id"></span></p>
+                <p style="margin: 5px 0;"><strong>Issued Through:</strong> <span id="type"></span></p>
+            </div>
+        </div>
+
+        <div>
+            <div style="background-color: #1E3166; color: white; padding: 10px; font-weight: bold; text-transform: uppercase;">Fare Information</div>
+            <div style="padding: 10px;">
+                <p style="margin: 5px 0;"><strong>Transaction Status:</strong> Payment Failed</p>
+                <p style="margin: 5px 0;"><strong>Flight Fare:</strong> <span id="price"></span> <span>BTN</span></p>
+                <p style="margin: 5px 0;"><strong>Transaction Description:</strong> DrukAir Heli Reservation System Payment</p>
+            </div>
+        </div>
+        <div style="text-align: center; margin-top: 20px;">
+            <button 
+                onclick="window.location.href='/'" 
+                style="background-color: #1E3166; color: white; border: none; padding: 10px 20px; border-radius: 5px; font-size: 16px; cursor: pointer;">
+                OK
+            </button>
+        </div>
+    </div>
+</body>
+<script>
+    document.addEventListener("DOMContentLoaded", async function () {
+        const id = sessionStorage.getItem("bID");
+        try {
+            const getbooking = await axios({
+                method: "GET",
+                url: \`https://heli.drukair.com.bt/api/bookings/\${id}\`
+            });
+    
+            const booking = getbooking.data.data;
+            document.getElementById("name").textContent = booking.agent_name;
+            document.getElementById("cid").textContent = booking.agent_cid;
+            document.getElementById("id").textContent = booking.bookingID;
+            document.getElementById("type").textContent = booking.booking_type;
+            document.getElementById("price").textContent = booking.bookingPriceBTN;
+        } catch (err) {
+            console.log(err);
+        }
+    });      
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js"></script>
+</html>
+`)
+});
+
+app.post('/successrmapayment', (req, res) => {
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Confirmation</title>
+</head>
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f9f9f9;">
+    <div style="max-width: 600px; margin: 20px auto; padding: 20px; border: 1px solid #ccc; background-color: #fff; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+        <h2 style="text-align: center; color: #333;">PAYMENT CONFIRMATION (<span style="color: #E87111;">DRUKAIR HELI-RESERVATION</span>)</h2>
+        <hr style="border: none; border-top: 1px solid #ccc; margin: 10px 0;">
+        
+        <div style="margin-bottom: 20px;">
+            <div style="background-color: #1E3166; color: white; padding: 10px; font-weight: bold; text-transform: uppercase;">Agent Information</div>
+            <div style="padding: 10px;">
+                <p style="margin: 5px 0;"><strong>Name:</strong> <span id="name"></span></p>
+                <p style="margin: 5px 0;"><strong>Agent CID:</strong> <span id="cid"></span></p>
+                <p style="margin: 5px 0;"><strong>Booking Reference:</strong> <span id="id"></span></p>
+                <p style="margin: 5px 0;"><strong>Issued Through:</strong> <span id="type"></span></p>
+            </div>
+        </div>
+
+        <div>
+            <div style="background-color: #1E3166; color: white; padding: 10px; font-weight: bold; text-transform: uppercase;">Fare Information</div>
+            <div style="padding: 10px;">
+                <p style="margin: 5px 0;"><strong>Transaction Status:</strong> Payment Successful</p>
+                <p style="margin: 5px 0;"><strong>Flight Fare:</strong> <span id="price"></span> <span>BTN</span></p>
+                <p style="margin: 5px 0;"><strong>Transaction Description:</strong> DrukAir Heli Reservation System Payment</p>
+            </div>
+        </div>
+        <div style="text-align: center; margin-top: 20px;">
+            <button 
+                onclick="window.location.href='/'" 
+                style="background-color: #1E3166; color: white; border: none; padding: 10px 20px; border-radius: 5px; font-size: 16px; cursor: pointer;">
+                OK
+            </button>
+        </div>
+    </div>
+</body>
+<script>
+    const update = async (id, booking) => {
+        try {
+          const res = await axios({
+            method: "PATCH",
+            url: \`https://heli.drukair.com.bt/api/bookings/\${id}\`,
+            data: {
+                payment_status: "Paid",
+                cType: "BTN",
+                price: booking?.bookingPriceBTN
+            },
+          });
+        } catch (err) {
+          console.log(err);
+        }
+    };      
+    document.addEventListener("DOMContentLoaded", async function () {
+        const id = sessionStorage.getItem("bID");
+        try {
+            const getbooking = await axios({
+                method: "GET",
+                url: \`https://heli.drukair.com.bt/api/bookings/\${id}\`
+            });
+    
+            const booking = getbooking.data.data;
+            document.getElementById("name").textContent = booking.agent_name;
+            document.getElementById("cid").textContent = booking.agent_cid;
+            document.getElementById("id").textContent = booking.bookingID;
+            document.getElementById("type").textContent = booking.booking_type;
+            document.getElementById("price").textContent = booking.bookingPriceBTN;
+            update(id, booking);
+        } catch (err) {
+            console.log(err);
+        }
+    });      
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js"></script>
+</html>
+`)
 });
 
 module.exports = app;
--- controllers/authController.js
+++ controllers/authController.js
@@ -63,8 +63,9 @@ const createSendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
         ),
-        httpOnly: false,
-        path: '/',
+        httpOnly: true,
+        sameSite: "Strict",
+        path: "/",
     }
     res.cookie('jwt', token, cookieOptions)
 
